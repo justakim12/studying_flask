f4f06e930690de3b140a76e8216a95f3a8546f47
David K. Kim <rlaruddlf711@naver.com>
2021-12-19 21:55:38 +0900

    Add user function

--- flask_api/main.py
+++ flask_api/main.py
@@ -8,5 +8,15 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/user')
+def hello_user():
+    return 'Hello, User!'
+
+
+@app.route('/user/<userName>')
+def hello_specific_user(userName):
+    return 'Hello, %s'%(userName)
+
+
 if __name__ == "__main__":
     app.run()
